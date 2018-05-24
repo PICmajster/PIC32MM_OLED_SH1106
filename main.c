9c7091f7fe03a9dd94bc70c4e3077c727237544c
wat <wat@wat-EP43T-UD3L>
2018-05-24 19:14:16 +0200

    stworzenie szablonu plików konfiguracyjnych

---
+++ main.c
@@ -0,0 +1,29 @@
+/*
+    File Name:        :  main.c
+    Device            :  PIC32MM0256GPM048
+    Compiler          :  XC32 2.05
+    MPLAB             :  MPLAB X 4.15
+    Created by        :  http://strefapic.blogspot.com
+*/
+#include "mcu_config_files/mcc.h"
+
+
+int main(void)
+{
+    // initialize the device
+    SYSTEM_Initialize();
+
+    // When using interrupts, you need to set the Global Interrupt Enable bits
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    //INTERRUPT_GlobalEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalDisable();
+
+    while (1)
+    {
+        // Add your application code
+    }
+}
