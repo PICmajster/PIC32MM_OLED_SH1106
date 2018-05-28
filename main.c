e164ac6fe54a1de05a674f71f03ba5acc74a696c
wat <wat@wat-EP43T-UD3L>
2018-05-28 19:34:09 +0200

    odświeżanie

--- main.c
+++ main.c
@@ -16,11 +16,11 @@ int main(void)
     SYSTEM_Initialize();
     initDisplay(); /*Alleluja*/
 
-        strFont5XY("HELLO WORLD", 30, 1) ;
+    //strFont5XY("HELLO WORLD", 30, 1) ;
     while (1)
     {
-//        sendCmd(0xAE) ;
-//        delayMs(100) ;
+        strFont5XY("HELLO WORLD", 30, 1) ;
+        delayMs(1000) ;
     }
 }
 
